96651eaae5b67f21f7432ce8e64f119f5cdfc691
DreadfulCrazy <lucasospina97@gmail.com>
2020-04-05 00:54:44 -0500

    [ADD] Context changed

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,22 +8,22 @@ const morgan = require("morgan");
 require("express-async-errors");
 const { NotFoundMiddleware, ErrorMiddleware } = require("../middlewares");
 
-module.exports = function name({HomeRoutes, UserRoutes}) {
+module.exports = function name({ HomeRoutes, UserRoutes }) {
     const router = express.Router();
     const apiRoutes = express.Router();
 
     //default middlewares
     apiRoutes
-    .use(express.json())
-    .use(cors())
-    .use(helmet())
-    .use(compression())
-    .use(morgan("dev"));
+        .use(express.json())
+        .use(cors())
+        .use(helmet())
+        .use(compression())
+        .use(morgan("dev"));
 
-    apiRoutes.use("/home",HomeRoutes);
-    apiRoutes.use("/user",UserRoutes);
+    apiRoutes.use("/home", HomeRoutes);
+    apiRoutes.use("/user", UserRoutes);
 
-    router.use("/v1/api",apiRoutes);
+    router.use("/ido-speak/v1/api", apiRoutes);
 
 
     router.use(NotFoundMiddleware);
